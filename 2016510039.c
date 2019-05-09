8f46625db64f68e45fd9e586db924a057281b23e
cme2002 <cme2002@osboxes>
2019-05-09 16:20:37 +0300

    second commit

--- 2016510039.c
+++ 2016510039.c
@@ -2,64 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> 
-struct _Json {
-	char *datadat;
-    char *indexndx;
-	int reclength;
-    char *keyEncoding;
-    int keyStart;
-    int keyEnd;
-    char *order;	
-};
-typedef struct _Json JsonOBJ;
- 
-void json_parse(json_object * jobj) {
-    
-    //JsonOBJ jsonobj;
-    int i=0;
-    char array[7][20];
-    void *ptr;
-    enum json_type type;
-    json_object_object_foreach(jobj, key, val) {
-    type = json_object_get_type(val);
-    switch (type) {
-        case  json_type_string:
-            printf("type: json_type_string, ");
-            printf("value: %s\n", json_object_get_string(val));
-            
-           // array[i]=val;
-            i++;
-        break;
-		case json_type_int:
-		    printf("type: json_type_int, ");
-            printf("value: %d\n", json_object_get_int(val));
-		break;
-        case json_type_boolean:
-		    printf("type: json_type_boolean, ");
-            printf("value: %b\n", json_object_get_int(val));
-		break;
-        case json_type_double:
-		    printf("type: json_type_double, ");
-            printf("value: %f\n", json_object_get_double(val));
-		break;
-        case json_type_array:
-		    printf("type: json_type_array, ");
-            printf("value: %s\n", json_object_get_array(val));
-		break;
-        case json_type_object:
-		    printf("type: json_type_object, ");
-            printf("value: %s\n", json_object_get_object(val));
-		break;
+#include<json-c/json.h>
+
+    struct json_object *parsed_json;
+	struct json_object *FileName;
+	struct json_object *indexName;
+	struct json_object *recordLength;
+	struct json_object *keyEncoding;
+	struct json_object *keyStart;
+    struct json_object *keyEnd;
+    struct json_object *order;
+
+int main(int argc, char **argv) {
+	FILE *fp;
+	char buffer[1024];
+   
+
+	size_t i;	
+
+	fp = fopen("j.json","r");
+	fread(buffer, 1024, 1, fp);
+	fclose(fp);
+
+	parsed_json = json_tokener_parse(buffer);
+
+	json_object_object_get_ex(parsed_json, "dataFileName", &FileName);
+	json_object_object_get_ex(parsed_json, "indexFileName", &indexName);
+	json_object_object_get_ex(parsed_json, "recordLength", &recordLength);
+    json_object_object_get_ex(parsed_json, "keyEncoding", &keyEncoding);
+    json_object_object_get_ex(parsed_json, "recordLength", &recordLength);
+    json_object_object_get_ex(parsed_json, "keyStart", &keyStart);
+    json_object_object_get_ex(parsed_json, "keyEnd", &keyEnd);
+    json_object_object_get_ex(parsed_json, "order", &order);
+
+	printf("Name: %s\n", json_object_get_string(FileName));
+	printf("Age: %d\n", json_object_get_int(keyStart));
 
 
-        }
-    }
-}
-
-int main() {
-    char * string = "{ \"dataFileName\" : \"data.dat\", \"indexFileName\" : \"data.ndx\", \"recordLength\" : 401, \"keyEncoding\" : \"UTF\", \"keyStart\" : 13, \"keyEnd\" : 23, \"order\" : \"ACS\" }";
-    printf ("JSON string: %s\n", string);
-    json_object * jobj = json_tokener_parse(string);
-    json_parse(jobj);
-    return 0;
 }
